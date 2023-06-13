d3e14ea736aa5d9bde0969a9134abf32915f7f6c
Prideland-Okoi <prideland.okoi@gmail.com>
2023-06-13 16:22:28 +0100

    Update 8-write_to_file.py

--- 8-write_to_file.py
+++ 8-write_to_file.py
@@ -2,6 +2,6 @@
 
 import os
 
-file = os.path.join('.', 'vivo.txt')
+file = os.path.join('.', 'seabeasts.txt')
 with open(file, 'w') as f:
-    f.write('1. Keep the Beat\n2. My Own Drum\n3. One more Song')
+    f.write('Captain Crow – Nell Benjamin & Laurence O’Keefe')
